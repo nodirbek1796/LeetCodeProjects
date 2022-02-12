4e812412adcc968368a906b64e8a3422c57a0358
Nodirbek_Xaytiov <nodirbekkhayitov1796@gmail.com>
2022-02-12 15:26:04 +0500

    I have done second task!

--- src/main/java/NoteBook.java
+++ src/main/java/NoteBook.java
@@ -1,5 +1,23 @@
+import SecondTaskInNUU.ClassOrderAndDistance;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class NoteBook {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        ArrayList<ClassOrderAndDistance> arrayList = new ArrayList<>();
+
+        arrayList.add(new ClassOrderAndDistance(1, 7));
+        arrayList.add(new ClassOrderAndDistance(1, 10));
+        arrayList.add(new ClassOrderAndDistance(1, 2));
+        arrayList.add(new ClassOrderAndDistance(1, 2));
+        arrayList.add(new ClassOrderAndDistance(1, 4));
+
+        System.out.println(arrayList);
+
+        arrayList.sort((o1, o2) -> (int) (o1.getDistance() - o2.getDistance()));
+        System.out.println(arrayList);
     }
 }
--- src/main/java/notebook/WorkBook.java
+++ src/main/java/notebook/WorkBook.java
@@ -1,12 +1,12 @@
 package notebook;
 
-import java.io.IOException;
+import java.util.HashSet;
 
 public class WorkBook {
-    public static void main(String[] args) throws IOException {
-        Employee employee = new Employee("John Smith", "+998932259747");
-        Employee employee1 = new Employee("Ross Geller", "+998946594488");
+    public static void main(String[] args) {
+        HashSet<Integer> uniqueNumbers = new HashSet<>();
 
-        System.out.println(employee);
+        System.out.println(uniqueNumbers.add(10));
+        System.out.println(uniqueNumbers.add(10));
     }
 }
