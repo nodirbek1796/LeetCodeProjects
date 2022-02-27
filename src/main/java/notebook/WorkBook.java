923786df80db1431464bdb5b2a48a35ca2d50147
Nodirbek_Xaytiov <nodirbekkhayitov1796@gmail.com>
2022-02-27 22:36:19 +0500

    I have done second task!

--- src/main/java/notebook/WorkBook.java
+++ src/main/java/notebook/WorkBook.java
@@ -1,12 +1,37 @@
 package notebook;
 
-import java.util.HashSet;
-
 public class WorkBook {
     public static void main(String[] args) {
-        HashSet<Integer> uniqueNumbers = new HashSet<>();
+        int[] numbers = {10, 15, 7, 3, 1};
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
 
-        System.out.println(uniqueNumbers.add(10));
-        System.out.println(uniqueNumbers.add(10));
+        bubbleSort(numbers, numbers.length);
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+
+    static void bubbleSort(int arr[], int n)
+    {
+        if (n == 1)                     //passes are done
+        {
+            return;
+        }
+
+        for (int i=0; i<n-1; i++)       //iteration through unsorted elements
+        {
+            if (arr[i] > arr[i+1])      //check if the elements are in order
+            {                           //if not, swap them
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+
+        bubbleSort(arr, n-1);           //one pass done, proceed to the next
     }
 }
